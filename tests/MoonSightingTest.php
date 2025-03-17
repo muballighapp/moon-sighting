81d65396079a7cfccbf67e154cb8e9f9501ea256
Rizwan Ahmad <rizwan_ranjha@hotmail.com>
2025-03-17 15:11:32 +0500

    updated to use Muballigh Packages.
    updated to use Muballigh packages.
    Muballigh\MoonSighting
    
    Co-Authored-By: Muballigh <202171617+muballighapp@users.noreply.github.com>
    Co-Authored-By: expertsols <196841543+expertsols@users.noreply.github.com>

--- tests/MoonSightingTest.php
+++ tests/MoonSightingTest.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace IslamicNetwork\MoonSighting\Tests\Unit;
+namespace Muballigh\MoonSighting\Tests\Unit;
 
-use IslamicNetwork\MoonSighting\Fajr;
-use IslamicNetwork\MoonSighting\Isha;
+use Muballigh\MoonSighting\Fajr;
+use Muballigh\MoonSighting\Isha;
 use PHPUnit\Framework\TestCase;
 use DateTime;
 
@@ -17,6 +17,9 @@ class MoonSightingTest extends TestCase
         $this->assertEquals('north', $pt->hemisphere);
         $this->assertEquals(88, $pt->getMinutesBeforeSunrise());
 
+        echo "\nDYY: " . $pt->getDyy();
+        echo "\nHemisphere: " . $pt->hemisphere;
+        echo "\nMinutes Before Sunrise: " . $pt->getMinutesBeforeSunrise();
 
         $date = new DateTime('24-11-2020'); // 338 days after dec 21 in the previous year
         $ptx = new Fajr($date, 25.2119894);
@@ -33,6 +36,9 @@ class MoonSightingTest extends TestCase
         $this->assertEquals('north', $pt->hemisphere);
         $this->assertEquals(86, $pt->getMinutesAfterSunset());
 
+        echo "\nDYY: " . $pt->getDyy();
+        echo "\nHemisphere: " . $pt->hemisphere;
+        echo "\nMinutes Before Sunrise: " . $pt->getMinutesAfterSunset();
 
         $date = new DateTime('24-11-2020'); // 338 days after dec 21 in the previous year
         $ptx = new Isha($date, 25.2119894);
@@ -49,6 +55,9 @@ class MoonSightingTest extends TestCase
         $this->assertEquals('south', $pt->hemisphere);
         $this->assertEquals(90, $pt->getMinutesBeforeSunrise());
 
+        echo "\nDYY: " . $pt->getDyy();
+        echo "\nHemisphere: " . $pt->hemisphere;
+        echo "\nMinutes Before Sunrise: " . $pt->getMinutesBeforeSunrise();
 
         $date = new DateTime('24-05-2020'); // 337 days after jun 21 in the previous year
         $ptx = new Fajr($date, -29.8586804);
@@ -65,6 +74,9 @@ class MoonSightingTest extends TestCase
         $this->assertEquals('south', $pt->hemisphere);
         $this->assertEquals(89, $pt->getMinutesAfterSunset());
 
+        echo "\nDYY: " . $pt->getDyy();
+        echo "\nHemisphere: " . $pt->hemisphere;
+        echo "\nMinutes Before Sunrise: " . $pt->getMinutesAfterSunset();
 
         $date = new DateTime('24-05-2020'); // 337 days after jun 21 in the previous year
         $ptx = new Isha($date, -29.8586804);
